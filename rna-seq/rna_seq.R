598c2f0643ea0a02f8f7f48f8fcc4509750685ba
Elphie1185 <delphine_rabiller@yahoo.fr>
2020-06-19 11:31:31 +0100

    initial commit

---
+++ rna-seq/rna_seq.R
@@ -0,0 +1,12 @@
+##################################################
+#
+# Performing Quantitative RNAseq
+# 
+# Bioinformatics analysis using RNAseq data usually starts with a read QC step
+# Then alugnment to a reference
+# Transcript abundances are estimates with read couting and statistical models
+# Differential expression between samples is assessed
+#
+##################################################
+
+# Which transcripts are differentially expressed?
